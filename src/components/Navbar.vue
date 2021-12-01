1fb16d21459d3d3d09f7d1e430438aad595b7d20
ShiftyGOSU <tom_karlsson94@hotmail.com>
2021-12-01 16:40:28 +0100

    Add navbar, method for api calls, simple print products

---
+++ src/components/Navbar.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="navbar">
+    <ul>
+      <li><a href="#">Hem</a></li>
+      <li><a href="#Products">Produkter</a></li>
+      <li><a href="#About">Om oss</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+/**
+ * Navigationbar component. On bigger screens the bar is in a fixed position at the top.
+ * Changes style and position to fixed at the bottom when the screen is smaller.
+ */
+export default {
+  components: {},
+  name: "Navbar",
+};
+</script>
+
+<style scoped>
+.navbar {
+  width: 100%;
+  background-color: #294c53;
+  position: fixed;
+  z-index: 10;
+  bottom: 0;
+  max-height: 6em;
+}
+ul {
+  display: flex;
+  justify-content: space-evenly;
+  gap: 1em;
+}
+
+li {
+  list-style: none;
+}
+
+a {
+  font-size: 18px;
+  color: white;
+  text-decoration: none;
+}
+
+@media screen and (min-width: 360px) {
+}
+
+@media screen and (min-width: 768px) {
+  .navbar {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    top: 0;
+    background-color: #53787f;
+  }
+}
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,42 @@
 import { createStore } from "vuex";
+// för att importera separata js filer
+import * as consts from "../assets/const.js";
 
 export default createStore({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+  state: {
+    errorMessage: String,
+    allProducts: [],
+  },
+  //Sync methods, called with this.$store.commit("methodName", Object);
+  mutations: {
+    setAllProducts(state, listOfProducts) {
+      state.allProducts = listOfProducts;
+    },
+  },
+  //Async methods this.$store.dispatch("methodName", Object);
+  actions: {
+    //--------------------------------------------------------------------------
+    async apiCall(state, urlApi) {
+      let json = null;
+
+      let apiUrl = consts.urls.urlHost + urlApi;
+
+      try {
+        let response = await fetch(apiUrl);
+        if (!response.ok) {
+          throw new Error("Something went wrong");
+        }
+        json = await response.json();
+      } catch (err) {
+        this.errorMessage = err.message;
+      }
+
+      return json;
+    },
+    //--------------------------------------------------------------------------
+    async getAllProducts() {
+      let json = await this.dispatch("apiCall", "products");
+      this.commit("setAllProducts", json);
+    },
+  },
 });
