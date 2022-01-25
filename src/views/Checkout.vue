f688704660891889dcb028edef07f80e1ac2d7b4
Niklas Johansson <sirniklasjohansson@gmail.com>
2022-01-25 16:46:56 +0100

    add cart products to checkout page

--- src/views/Checkout.vue
+++ src/views/Checkout.vue
@@ -1,13 +1,43 @@
 <template>
-  <div>
-    <TopTitle msg="Checkout test" />
+  <div class="container">
+    <div class="box left-box">
+      <div class="header-container">
+        <div class="item-container">
+          <p class="cart-footer-header header-text">Produkt</p>
+        </div>
+        <div class="quantity-price-container">
+          <div class="quantity-container">
+            <p class="cart-footer-header header-text">Antal</p>
+          </div>
+          <div class="price-container">
+            <p class="cart-footer-header header-text">Pris</p>
+          </div>
+        </div>
+      </div>
+      <CheckoutCartItem
+        v-for="itemInCart in $store.state.cartWithQuantity"
+        :key="itemInCart.product.productId"
+        :itemInCart="itemInCart"
+      />
+      <div class="footer-container">
+        <p class="cart-footer-header">
+          Att betala: {{ calculateTotalPrice }} kr
+        </p>
+      </div>
+    </div>
+    <div class="box right-box">
+      <button class="btn btn-primary" @Click="handlePayClick()">Betala</button>
+    </div>
   </div>
-  <button @Click="handlePayClick()">Betala</button>
 </template>
 
 <script>
+import CheckoutCartItem from "../components/CheckoutCartItem.vue";
+
 export default {
-  components: {},
+  components: {
+    CheckoutCartItem,
+  },
   methods: {
     handlePayClick() {
       //let userId = this.$store.state.user.userId;
@@ -15,11 +45,86 @@ export default {
       this.$store.dispatch("checkoutApiCall", url);
     },
   },
+  computed: {
+    calculateTotalPrice() {
+      let sum = 0;
+
+      this.$store.state.productsInCart.forEach((element) => {
+        sum += element.price;
+      });
+      return sum;
+    },
+  },
 };
 </script>
 
 <style scoped>
-button {
-  margin-top: 6em;
+.container {
+  display: flex;
+  padding: 0px;
+}
+
+.box {
+  flex-grow: 1;
+  justify-content: center;
+  align-items: center;
+}
+
+.left-box {
+}
+
+.right-box {
+}
+
+.header-container {
+  display: flex;
+  border-bottom: 1px solid grey;
+}
+
+.header-container div p {
+  float: left;
+  padding: 12px 12px 12px 0.5em;
+  color: black;
+}
+
+.item-container {
+  flex-grow: 7;
+}
+
+.quantity-price-container {
+  display: flex;
+  width: 10em;
+}
+
+.quantity-price-container div p {
+  float: left;
+  padding: 12px 12px 12px 0.5em;
+}
+
+.quantity-container {
+  display: flex;
+  align-items: center;
+  flex-grow: 1;
+}
+
+.price-container {
+  display: flex;
+  align-items: center;
+  flex-grow: 2;
+}
+
+.footer-container {
+  display: flex;
+  align-items: center;
+  border-top: 1px solid grey;
+}
+
+.cart-footer-header {
+  color: black;
+  font-weight: bold;
+  padding: 12px 16px;
+  text-decoration: none;
+  margin-bottom: 0;
+  flex-grow: 1;
 }
 </style>
